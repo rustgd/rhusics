96b1f4e6091a72a10161655fa3415382c1395d35
Simon Rönnberg <simon.ronnberg@codemill.se>
2017-09-15 22:54:48 +0200

    Refactoring 2d polygon to use a better algorithm for finding the furthest point

--- src/collide/primitive2d.rs
+++ src/collide/primitive2d.rs
@@ -160,9 +160,87 @@ impl Primitive for Primitive2 {
             }
 
             Primitive2::ConvexPolygon(ConvexPolygon { ref vertices, .. }) => {
-                ::util::get_max_point(vertices, direction, transform)
+                if vertices.len() < 10 {
+                    ::util::get_max_point(vertices, direction, transform)
+                } else {
+                    get_max_point(vertices, direction, transform)
+                }
             }
         }
+    }
+}
+
+fn get_max_point<P, T>(vertices: &Vec<P>, direction: &P::Diff, transform: &T) -> P
+where
+    P: EuclideanSpace<Scalar = Real>,
+    T: Pose<P>,
+{
+    let direction = transform.inverse_rotation().rotate_vector(*direction);
+
+    // figure out where to start, if the direction is negative for the first vertex,
+    // go halfway around the polygon
+    let mut start_index : i32 = 0;
+    let mut max_dot = vertices[0].dot(direction);
+    if max_dot < P::Scalar::zero() {
+        start_index = vertices.len() as i32 / 2;
+        max_dot = dot_index(vertices, start_index, &direction);
+    }
+
+    let left_dot = dot_index(vertices, start_index - 1, &direction);
+    let right_dot = dot_index(vertices, start_index + 1, &direction);
+
+    // check if start is highest
+    let p = if start_index == 0 && max_dot > left_dot && max_dot > right_dot {
+        vertices[0]
+    } else {
+        // figure out iteration direction
+        let mut add : i32 = 1;
+        let mut previous_dot = left_dot;
+        if left_dot > max_dot && left_dot > right_dot {
+            add = -1;
+            previous_dot = right_dot;
+        }
+
+        // iterate
+        let mut index = start_index + add;
+        let mut current_dot = max_dot;
+        if index == vertices.len() as i32 {
+            index = 0;
+        }
+        if index == -1 {
+            index = vertices.len() as i32 - 1;
+        }
+        while index != start_index {
+            let next_dot = dot_index(vertices, index + add, &direction);
+            if current_dot > previous_dot && current_dot > next_dot {
+                break;
+            }
+            previous_dot = current_dot;
+            current_dot = next_dot;
+            index += add;
+            if index == vertices.len() as i32 {
+                index = 0;
+            }
+            if index == -1 {
+                index = vertices.len() as i32 - 1;
+            }
+        }
+        vertices[index as usize]
+    };
+
+    *transform.position() + transform.rotation().rotate_point(p).to_vec()
+}
+
+#[inline]
+fn dot_index<P>(vertices: &Vec<P>, index: i32, direction: &P::Diff) -> Real
+where P : EuclideanSpace<Scalar = Real> {
+    let index_u = index as usize;
+    if index_u == vertices.len() {
+        vertices[0].dot(*direction)
+    } else if index == -1 {
+        vertices[vertices.len() - 1].dot(*direction)
+    } else {
+        vertices[index_u].dot(*direction)
     }
 }
 
@@ -174,6 +252,7 @@ mod tests {
 
     use super::*;
     use BodyPose;
+    use collide2d::BodyPose2;
 
     // circle
     #[test]
@@ -232,5 +311,40 @@ mod tests {
     // util
     fn bound(min_x: Real, min_y: Real, max_x: Real, max_y: Real) -> Aabb2<Real> {
         Aabb2::new(Point2::new(min_x, min_y), Point2::new(max_x, max_y))
+    }
+
+    #[test]
+    fn test_max_point() {
+        let vertices = vec![
+            Point2::new(5., 5.),
+            Point2::new(4., 6.),
+            Point2::new(3., 7.),
+            Point2::new(2., 6.),
+            Point2::new(1., 6.),
+            Point2::new(0., 5.),
+            Point2::new(-1., 4.),
+            Point2::new(-3., 3.),
+            Point2::new(-6., 1.),
+            Point2::new(-5., 0.),
+            Point2::new(-4., -1.),
+            Point2::new(-2., -3.),
+            Point2::new(0., -7.),
+            Point2::new(1., -8.),
+            Point2::new(2., -5.),
+            Point2::new(3., 0.),
+            Point2::new(4., 3.),
+        ];
+        let transform = BodyPose2::one();
+        let point = get_max_point(&vertices, &Vector2::new(-1., 0.), &transform);
+        assert_eq!(Point2::new(-5., 0.), point);
+
+        let point = get_max_point(&vertices, &Vector2::new(0., -1.), &transform);
+        assert_eq!(Point2::new(1., -8.), point);
+
+        let point = get_max_point(&vertices, &Vector2::new(0., 1.), &transform);
+        assert_eq!(Point2::new(3., 7.), point);
+
+        let point = get_max_point(&vertices, &Vector2::new(1., 0.), &transform);
+        assert_eq!(Point2::new(5., 5.), point);
     }
 }
