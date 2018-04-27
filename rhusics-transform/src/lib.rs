403316908fc7ab11dc13f3a51361d6665cb5d187
Simon Rönnberg <seamonr@gmail.com>
2018-04-27 13:32:12 +0200

    feat: Implement Pose for cgmath::Decomposed

--- rhusics-transform/src/lib.rs
+++ rhusics-transform/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate cgmath;
 extern crate collision;
 
-use cgmath::{EuclideanSpace, Transform};
+use cgmath::{BaseFloat, Decomposed, EuclideanSpace, One, Rotation, Transform};
 pub use collision::{Interpolate, TranslationInterpolate};
 
 /// Pose abstraction
@@ -23,4 +23,35 @@ where
 
 pub trait PhysicsTime<S> {
     fn delta_seconds(&self) -> S;
+}
+
+impl<P, R> Pose<P, R> for Decomposed<P::Diff, R>
+where
+    P: EuclideanSpace,
+    P::Scalar: BaseFloat,
+    R: Rotation<P>,
+{
+    fn new(position: P, rotation: R) -> Self {
+        Decomposed {
+            rot: rotation,
+            disp: position.to_vec(),
+            scale: P::Scalar::one(),
+        }
+    }
+
+    fn set_rotation(&mut self, rotation: R) {
+        self.rot = rotation;
+    }
+
+    fn set_position(&mut self, position: P) {
+        self.disp = position.to_vec();
+    }
+
+    fn rotation(&self) -> R {
+        self.rot
+    }
+
+    fn position(&self) -> P {
+        P::from_vec(self.disp)
+    }
 }
